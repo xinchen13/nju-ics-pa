ec42a2ad392c53141111d3764c3df08fa08396f3
xinchen <fxc23@mails.tsinghua.edu.cn>
2023-11-23 10:53:29 +0800

    >  compile NEMU 13 xinchen Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:53:29 up 1 day,  6:07,  1 user,  load average: 0.23, 0.28, 0.27

--- nemu/src/utils/iringbuf.c
+++ nemu/src/utils/iringbuf.c
@@ -18,10 +18,11 @@ void write_data(iRingBuffer buffer, const char *data) {
 
 void print_buffer(iRingBuffer buffer) {
     for (int current_ptr = 0; current_ptr < IRINGBUF_SIZE; current_ptr++) {
-    if (current_ptr == buffer.write_ptr){
-        printf(" -. %s\n", buffer.data[current_ptr]);
-    }
-    else
-        printf("     %s\n", buffer.data[current_ptr]);
+        if (current_ptr == buffer.write_ptr) {
+            printf(" --> %s\n", buffer.data[current_ptr]);
+        }
+        else {
+            printf("     %s\n", buffer.data[current_ptr]);
+        }         
     }
 }

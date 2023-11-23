d1855ec4ee2b599cf8209e2f7425b253856ad733
xinchen <fanxinchen@whu.edu.cn>
2023-11-23 14:59:59 +0800

    update iringbuf

--- nemu/src/utils/iringbuf.c
+++ nemu/src/utils/iringbuf.c
@@ -25,29 +25,3 @@ void print_buffer(iRingBuffer *buffer) {
         printf("     %s\n", buffer->data[current_ptr]);
     }
 }
-
-// void init_buffer(iRingBuffer buffer) {
-//     for (int i = 0; i < IRINGBUF_SIZE; i++) {
-//         for (int j = 0; j < LOG_LEHGTH; j++) {
-//             buffer.data[i][j] = '\0';
-//         }
-//     }
-//     buffer.write_ptr = 0;
-// }
-
-// void write_buffer(iRingBuffer buffer, const char *log_buf) {
-//     buffer.write_ptr = (buffer.write_ptr + 1) % IRINGBUF_SIZE;
-//     strcpy(buffer.data[buffer.write_ptr], log_buf);
-//     printf("position: %d\n", buffer.write_ptr);
-// }
-
-// void print_buffer(iRingBuffer buffer) {
-//     for (int current_ptr = 0; current_ptr < IRINGBUF_SIZE; current_ptr++) {
-//         if (current_ptr == buffer.write_ptr) {
-//             printf(" --> %s\n", buffer.data[current_ptr]);
-//         }
-//         else {
-//             printf("     %s\n", buffer.data[current_ptr]);
-//         }         
-//     }
-// }
